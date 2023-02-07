d1b74f71e8c03fa5f5b0fe8072db566ee6366836
robby <robin.kw99@yahoo.com>
2023-02-08 01:22:25 +0530

    Setup of contact form routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const fs = require('fs');
+const nodemailer = require('nodemailer');
 const artwork = require('./media');
 const port = process.env.port || 3000;
 
@@ -17,6 +17,15 @@ app.use(appUsage);
 
 app.set('view engine', 'ejs');
 
+// nodemailer config
+const transporter = nodemailer.createTransport({
+	service: `hotmail`,
+	auth: {
+		user: process.env.SENDER,
+		pass: process.env.PASSWORD,
+	},
+});
+
 // home routes
 app.get('/', (req, res) => {
 	res.render('index');
@@ -29,6 +38,23 @@ app.get('/portfolio', (req, res) => {
 // contact routes
 app.get('/contact', (req, res) => {
 	res.render('contact');
+});
+
+// // get contact form details
+app.post('/contact', (req, res) => {
+	const { name, email, subject, message } = req.body;
+
+	const mailOptions = {
+		from: process.env.SENDER,
+		to: process.env.RECEIVER,
+		subject: subject,
+		text: `Hello, I am ${name}. My contact is ${email}. ${message}`,
+	};
+
+    transporter.sendMail(mailOptions,(err,info)=>{
+        let output = err || info.response
+        console.log(output);
+    })
 });
 
 //
